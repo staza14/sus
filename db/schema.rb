2e7a0d83c0c01a050535901cd2248447e131448b
Vasana Attanayake <vasana1997@hotmail.com>
2023-08-28 16:33:56 +0100

    added first name

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_143527) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_143737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_143527) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
