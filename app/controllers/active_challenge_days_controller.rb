b0df42c570233e33b67672ffc0725b6404b81ee7
Anastasia Florentis <anastasiaflorentis@gmail.com>
2023-08-31 13:25:02 +0100

    worked on dashboard

--- app/controllers/active_challenge_days_controller.rb
+++ app/controllers/active_challenge_days_controller.rb
@@ -2,5 +2,9 @@ class ActiveChallengeDaysController < ApplicationController
   def create
   end
   def update
+    @active_challenge_day = ActiveChallengeDay.find(params[:id])
+    @active_challenge_day.status = true
+    @active_challenge_day.save
+    head :ok
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,3 @@ end
     params.require(:user).permit(:first_name, :last_name)
   end
 end
-
