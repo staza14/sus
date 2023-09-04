524eec9add799e33f287c10dce408ab12e781281
Heloise Leblant <leblant@hotmail.fr>
2023-09-04 17:52:03 +0100

    fix search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :car_hours, :bus_hours, :train_hours, :cycle_hours, :foot_hours, :flight_long_hours, :flight_short_hours, :food, :waste, :source, :lights, :clothing, :temperature, :bath, :long_shower, :short_shower])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :car_hours, :bus_hours, :train_hours, :cycle_hours, :foot_hours, :flight_long_hours, :flight_short_hours, :food, :waste, :source, :lights, :clothing, :temperature, :bath, :long_shower, :short_shower])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :car_hours, :bus_hours, :train_hours, :cycle_hours, :foot_hours, :flight_long_hours, :flight_short_hours, :food, :waste, :source, :lights, :clothing, :temperature, :bath, :long_shower, :short_shower])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name , :avatar, :car_hours, :bus_hours, :train_hours, :cycle_hours, :foot_hours, :flight_long_hours, :flight_short_hours, :food, :waste, :source, :lights, :clothing, :temperature, :bath, :long_shower, :short_shower])
   end
 
   def after_sign_up_path_for(resource)
