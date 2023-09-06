115193c7d1fb6607927342d355a1af165ec0bcc6
Vasana Attanayake <vasana1997@hotmail.com>
2023-09-06 10:27:59 +0100

    formed

--- app/javascript/controllers/daily_log_controller.js
+++ app/javascript/controllers/daily_log_controller.js
@@ -4,32 +4,57 @@ export default class extends Controller {
   static targets = ["section"];
   static values = { currentSectionIndex: Number };
 
+  static sectionColors = {
+    "section-transportation": "#BBACEC",
+    "section-meals": "#80DBC1",
+    "section-home": "#FACDA4"
+  };
+
   connect() {
     console.log("sign up connected");
 
+    this.currentSectionColor = this.element.getAttribute("data-header-color");
+  }
+  showSection1(e) {
+    e.preventDefault();
+    this.sectionTargets[0].classList.remove("d-none");
+    this.sectionTargets[1].classList.add("d-none");
+
+    this.updateHeaderColor("section-transportation");
   }
 
   showSection2(e) {
     e.preventDefault();
     this.sectionTargets[0].classList.add("d-none");
     this.sectionTargets[1].classList.remove("d-none");
-  }
 
-  showSection1(e) {
-    e.preventDefault();
-    this.sectionTargets[0].classList.remove("d-none");
-    this.sectionTargets[1].classList.add("d-none");
+    this.updateHeaderColor("section-meals");
   }
 
   showSection3(e) {
     e.preventDefault();
     this.sectionTargets[1].classList.add("d-none");
     this.sectionTargets[2].classList.remove("d-none");
+
+    this.updateHeaderColor("section-home");
   }
 
   backToSection2(e) {
     e.preventDefault();
     this.sectionTargets[2].classList.add("d-none");
     this.sectionTargets[1].classList.remove("d-none");
+
+    this.currentSectionColor = "section-meals";
+    this.updateHeaderColor(this.currentSectionColor);
+  }
+
+  updateHeaderColor(newColorClass) {
+    const modalHeader = document.querySelector(".add-entry-form");
+    modalHeader.style.backgroundColor = this.constructor.sectionColors[newColorClass];
+    console.log(this.constructor.sectionColors[newColorClass])
+    console.log('--------')
+    console.log(modalHeader)
+    console.log(modalHeader.style.backgroundColor)
+    this.currentSectionColor = newColorClass;
   }
 }
