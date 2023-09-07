536d3b1a6f341e7c34aab0fea5f00324b8210e8a
Anastasia Florentis <anastasiaflorentis@gmail.com>
2023-09-07 11:04:06 +0100

    fix: after_save changed to after_update

--- app/models/active_challenge_day.rb
+++ app/models/active_challenge_day.rb
@@ -2,7 +2,7 @@ class ActiveChallengeDay < ApplicationRecord
   belongs_to :active_challenge
   default_scope { order(created_at: :asc) }
 
-  after_save :check_challenge_completion
+  after_update :check_challenge_completion
 
   def check_challenge_completion
     active_challenge = self.active_challenge
@@ -11,7 +11,6 @@ class ActiveChallengeDay < ApplicationRecord
     unless days.any? { |day| day.status == false }
       active_challenge.mark_as_completed
       user.add_challenge_points
-      # flash[:notice] = "Nice! You've completed #{active_challenge.challenge.name}."
     end
   end
 end
